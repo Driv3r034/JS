fda78017c2fbb7bd986bb822091d6efd2c10d811
Driv3r034 <64967374+Driv3r034@users.noreply.github.com>
2020-11-04 21:13:57 +0300

    test1

--- JS in HTML/Задача по переменным.js
+++ JS in HTML/Задача по переменным.js
@@ -248,8 +248,10 @@ console.log(mySecretNumber === harpoGuess);
 var grouchoGuess = 5;
 console.log(mySecretNumber === grouchoGuess);
 
-
-
+var stringNumber = "5";
+var actualNumber = 5;
+console.log(stringNumber === actualNumber);
+console.log(stringNumber == actualNumber);
 
 
 
